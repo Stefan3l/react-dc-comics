dd1162d03f58526911b86cc03728f79358f67e4d
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-01-28 16:29:09 +0100

    feat: main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,35 @@
 export default function Main() {
-  return <main></main>;
+  return (
+    <main>
+      <section className="main-section">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img src="./image/buy-comics-digital-comics.png" alt="DG" />
+              <h3>Digital Comics</h3>
+            </div>
+            <div className="col">
+              <img src="./image/buy-comics-merchandise.png" alt="merchendise" />
+              <h3>Dc Merchendise</h3>
+            </div>
+            <div className="col">
+              <img
+                src="./image/buy-comics-subscriptions.png"
+                alt="subscription"
+              />
+              <h3>Subscription</h3>
+            </div>
+            <div className="col">
+              <img src="./image/buy-comics-shop-locator.png" alt="locator" />
+              <h3>Comic Shop Locator</h3>
+            </div>
+            <div className="col">
+              <img src="./image/buy-dc-power-visa.svg" alt="power" />
+              <h3>Dc Power Visa</h3>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
